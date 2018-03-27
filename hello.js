5b640fa0ead33c7f57497f4a4b764a8d681337ab
sjs95117 <37557853+sjs95117@users.noreply.github.com>
2018-03-27 14:48:26 +0900

    .

--- hello.js
+++ hello.js
@@ -1,11 +1,11 @@
 var http = require('http');
 
 function requestCallbackFunction (req, res) {
-  // if(req.url === '/favicon.ico') {
-  //   console.log('Favicon was requested');
-  // } else {
+  if(req.url === '/favicon.ico') {
+     console.log('Favicon was requested');
+  } else {
     console.log('hello');
-  // }
+  }
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World!');
 }
